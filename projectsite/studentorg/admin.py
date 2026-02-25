e0ea8bffbda634049adb7abb22d5c914888b3123
D13TH3 <deatherabunto2@gmail.com>
2026-02-26 02:37:22 +0800

    “refactor admin.py”

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -1,10 +1,41 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import College, Program, Organization, Student, OrgMember
 
-admin.site.register(College)
-admin.site.register(Program)
-admin.site.register(Organization)
-admin.site.register(Student)
-admin.site.register(OrgMember)
+# Task A 
+@admin.register(College)
+class CollegeAdmin(admin.ModelAdmin):
+    list_display = ("college_name", "created_at", "updated_at")
+    search_fields = ("college_name",)
+    list_filter = ("created_at",)
+
+# Task B 
+@admin.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ("prog_name", "college")
+    search_fields = ("prog_name", "college__college_name")
+    list_filter = ("college",)
+
+# Task C
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ("name", "college", "description")
+    search_fields = ("name", "description")
+    list_filter = ("college",)
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("student_id", "lastname", "firstname", "middlename", "program")
+    search_fields = ("lastname", "firstname",)
+
+
+@admin.register(OrgMember)
+class OrgMemberAdmin(admin.ModelAdmin):
+    list_display = ("student", "get_member_program", "organization", "date_joined")
+    search_fields = ("student__lastname", "student__firstname")
+
+    def get_member_program(self, obj):
+        try:
+            return obj.student.program
+        except AttributeError:
+            return None
+    get_member_program.short_description = 'Program'
